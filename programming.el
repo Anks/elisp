3a0087d102cbedd7f16b16c9c21178a74ac040c8
Ankit Solanki <ankit.solanki@gmail.com>
2017-03-16 08:39:45 +0530

    Removing skewer for now

--- programming.el
+++ programming.el
@@ -49,23 +49,6 @@
   (setq js2-highlight-level 3) ; Highlight many built-in functions.
   (setq js2-global-externs '("require" "module" "console")))
 
-;; Skewer -- live web development minor mode.
-;; Methods of launching:
-;;   * M-x run-skewer
-;;   * Use the Skewer bookmarklet to inject it into an existing page.
-;; Keybindings resemble the Lisp ones:
-;;   C-x C-e -- JS: eval form (with prefix: insert result); CSS: load declaration.
-;;   C-M-x -- JS: eval top-level-form; CSS: load rule; HTML: load tag.
-;;   C-c C-k -- JS, CSS: eval buffer.
-;;   C-c C-z -- JS: switch to REPL (logging: "skewer.log()", like "console.log()").
-;; Forms are sent to all attached clients simultaneously (use `list-skewer-clients' to show them).
-;; If the browser disconnects, use "skewer()" in the browser console to reconnect.
-(use-package skewer-mode
-  :ensure t
-  :init
-  (skewer-setup)) ; Integrate with js2-mode, html-mode and css-mode. (Don't worry about performance, this function is in a separate file.)
-
-
 (use-package js2-refactor
   :ensure t
   :init
