3213fe490fa173ae566d94c3760f09bc0eaa5d90
Ankit Solanki <ankit.solanki@gmail.com>
2017-04-06 07:31:57 +0530

    Little optimisation for improving load times.

--- defaults.el
+++ defaults.el
@@ -1,6 +1,10 @@
 ;; Some of these are from magnar's sane-defaults.el
 ;; Also from technomancy/better-defaults
 
+;; via https://www.reddit.com/r/emacs/comments/4j828f/til_setq_gcconsthreshold_100000000/d34gbsp/
+(setq gc-cons-threshold 100000000)
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+
 ;; Remove all chrome
 ;; Do this early to avoid flashing
 (menu-bar-mode -1)
